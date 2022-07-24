b42fbbbd6e058445bd424658103645edd68e06ed
Maurício Naoki <mauricio.naoki@garnon.me>
2022-07-24 00:08:27 -0300

    second

---
+++ dist/js/272.1380c202.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvuetify"]=self["webpackChunkvuetify"]||[]).push([[272],{1272:function(t,u,n){n.r(u),n.d(u,{default:function(){return r}});var e=function(){var t=this;t._self._c;return t._m(0)},s=[function(){var t=this,u=t._self._c;return u("div",{staticClass:"about"},[u("h1",[t._v("This is an about page")])])}],i=n(1001),a={},l=(0,i.Z)(a,e,s,!1,null,null,null),r=l.exports}}]);
+//# sourceMappingURL=272.1380c202.js.map
